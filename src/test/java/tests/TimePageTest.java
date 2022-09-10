f1999299c635ecd90912df9057a8c46d3c1a8d76
pra4u <palkar.p@gamail.com>
2022-09-10 10:12:47 +0530

    added timepage and timepageTest

---
+++ src/test/java/tests/TimePageTest.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class TimePageTest {
+	
+	
+	@Test
+	public void test1()
+	{
+		//Test Steps1
+		System.out.println("TimePage");
+	}
+
+}
