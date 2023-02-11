5797f808f8a045a29a07f95ec87e8b93720916a3
pra4u <palkar.p@gamail.com>
2023-02-11 08:41:09 +0530

    updated constant

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -8,6 +8,8 @@ public class Constants {
 	//LoginPage
 	public static final String LOGIN_PAGE_TITLE="OrangeHRM";
 	
+	public static final String HOME_PAGE_TITLE="Home Page";
+	
 	//HomePage
 	public static final String HOME_PAGE_USERNAME="Paul Collings";
 	public static final List<String> HOME_PAGE_MENU_ITEMS=  Arrays.asList(new String[]{"Admin", "PIM", "Leave", "Time", "Recruitment", "My Info", "Performance", "Dashboard", "Directory","Maintenance","Buzz"});
